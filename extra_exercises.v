8dcf85a0bdd4ce10fd92a785d730aea52f4f07a0
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-08-04 23:31:30 -0400

    s

--- extra_exercises.v
+++ extra_exercises.v
@@ -2,20 +2,5 @@
  so that you can prove the following lemma: 
  forall n, myOdd n -> exists m, n = 2*m +1 *)
 
-Inductive myOdd : nat -> Set :=
-| m_1 : myOdd 1
-| m_IH : forall n : nat, myOdd n -> myOdd (S (S n)).
-
-Check myOdd.
-
-Lemma myOdd_lemma : forall n,
-  myOdd n -> (exists m, n = 2*m + 1).
-Proof.
-  intros n.
-  intros H.
-  induction n as [| n'].
-  Case "n = 0".
-
-
 (* 2. Define myOdd2 and myEven2 mutually inductively. 
 Prove the lemma: forall n, myOdd2 n -> exists m, n = 2*m +1. *)
