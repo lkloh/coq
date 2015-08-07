63583ab27d3e96b4b8b984aa04af30723ab2ff8e
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-08-07 16:00:01 -0400

    saving foo

--- MoreInd.v
+++ MoreInd.v
@@ -141,7 +141,7 @@ Check natlist_ind.
       forall P : natlist -> Prop,
          P nnil  ->
          (forall (n : nat) (l : natlist), P l -> P (ncons n l)) ->
-         forall n : natlist, P n *)
+         forall l : natlist, P l *)
 
 (** **** Exercise: 1 star, optional (natlist1)  *)
 (** Suppose we had written the above definition a little
@@ -152,8 +152,15 @@ Inductive natlist1 : Type :=
   | nsnoc1 : natlist1 -> nat -> natlist1.
 
 (** Now what will the induction principle look like? *)
-(**  
+(**
+  natlist1_ind:
+    forall P : natlist1 -> Prop,
+      P nnil1 ->
+      (forall (l : natlist1) (n : nat), P l -> P (nsnoc1 n l)) ->
+      forall l : natlist1, P 
 *)
+
+Check natlist1_ind.
 
 (** From these examples, we can extract this general rule:
 
@@ -181,8 +188,18 @@ Inductive byntree : Type :=
  | bempty : byntree  
  | bleaf  : yesno -> byntree
  | nbranch : yesno -> byntree -> byntree -> byntree.
-(** [] *)
+(**  
+  byntree_ind:
+    forall P : byntree -> Prop,
+      P bempty -> 
+      (forall (yn : yesno), P yn -> P (bleaf yn)) ->
+      (forall (yn : yesno) (bt1 : byntree), P bt1  
+         -> (forall (bt2 : byntree) P bt2) 
+         -> (nbranch yn bt1 bt2) )->
+      forall bt : byntree, P bt
+*)
 
+Check byntree_ind.
 
 (** **** Exercise: 1 star, optional (ex_set)  *)
 (** Here is an induction principle for an inductively defined
@@ -195,9 +212,10 @@ Inductive byntree : Type :=
     Give an [Inductive] definition of [ExSet]: *)
 
 Inductive ExSet : Type :=
-  (* FILL IN HERE *)
-.
-(** [] *)
+  | con1 : bool -> ExSet
+  | con2 : nat -> ExSet -> ExSet.
+
+Check ExSet_ind.
 
 (** What about polymorphic datatypes?
 
@@ -231,7 +249,13 @@ Inductive tree (X:Type) : Type :=
   | leaf : X -> tree X
   | node : tree X -> tree X -> tree X.
 Check tree_ind.
-(** [] *)
+(**  
+  tree_ind :
+    forall (X : Type) (P : tree X -> Prop),
+      (forall (x : X), P (leaf X x)) ->
+      (forall (t1 : tree X), P t1 -> forall (t2 : tree X), P t2 -> P (node X t1 t2) ) ->
+      forall (t : tree X), P t
+*)
 
 (** **** Exercise: 1 star, optional (mytype)  *)
 (** Find an inductive definition that gives rise to the
@@ -240,11 +264,15 @@ Check tree_ind.
         forall (X : Type) (P : mytype X -> Prop),
             (forall x : X, P (constr1 X x)) ->
             (forall n : nat, P (constr2 X n)) ->
-            (forall m : mytype X, P m -> 
-               forall n : nat, P (constr3 X m n)) ->
+            (forall m : mytype X, P m -> forall n : nat, P (constr3 X m n)) ->
             forall m : mytype X, P m                   
 *) 
-(** [] *)
+Inductive mytype (X : Type) : Type :=
+  | constr1 : X -> mytype X
+  | constr2 : nat -> mytype X
+  | constr3 : mytype X -> nat -> mytype X.
+
+Check mytype_ind.
 
 (** **** Exercise: 1 star, optional (foo)  *)
 (** Find an inductive definition that gives rise to the
@@ -253,11 +281,15 @@ Check tree_ind.
         forall (X Y : Type) (P : foo X Y -> Prop),
              (forall x : X, P (bar X Y x)) ->
              (forall y : Y, P (baz X Y y)) ->
-             (forall f1 : nat -> foo X Y,
-               (forall n : nat, P (f1 n)) -> P (quux X Y f1)) ->
+             (forall f1 : nat -> foo X Y, (forall n : nat, P (f1 n)) -> P (quux X Y f1)) ->
              forall f2 : foo X Y, P f2       
 *) 
-(** [] *)
+Inductive foo (X Y : Type) : Type :=
+  | bar : X -> foo X Y
+  | baz : Y -> foo X Y
+  | quux : (nat -> foo X Y) -> foo X Y.
+
+Check foo_ind.
 
 (** **** Exercise: 1 star, optional (foo')  *)
 (** Consider the following inductive definition: *)
