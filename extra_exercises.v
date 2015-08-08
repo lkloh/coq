4c7b2aa2927fd0f70a8a0d6d91ae88a588dd5421
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-08-08 16:30:48 -0400

    dumb proof

--- extra_exercises.v
+++ extra_exercises.v
@@ -28,6 +28,16 @@ Proof.
     intros m H. rewrite -> H. reflexivity.
 Qed.
 
+Theorem plus_n_Sm : forall n m : nat, 
+  n + (S m) = S (n + m).
+Proof.
+  admit.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+  Admitted.
+
 Lemma helper : forall (n x : nat),
   n = 2 * x + 1 -> S (S n) = 2 * (x + 1) + 1.
 Proof.
@@ -40,10 +50,35 @@ Proof.
     intros x H.
       destruct x.
       simpl in H. simpl. rewrite -> H. reflexivity.
-      simpl. apply n_eq_m__Sn_eq_Sm.
-      rewrite -> plus_0_r.
-      simpl in H. rewrite -> plus_0_r in H. 
-  
+      simpl. rewrite -> plus_0_r. 
+      apply n_eq_m__Sn_eq_Sm.
+      assert (HH : x + 1 + S (x + 1) + 1 = 2 * (x + 1) + 1).
+        admit.
+   admit.
+Qed.
+
+Theorem plus_swap : forall n m p : nat, 
+  n + (m + p) = m + (n + p).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+  Lemma expand_S : forall n : nat,
+    S n = n + 1.
+  Proof.
+    intros n.
+    induction n.
+    reflexivity.
+    simpl. rewrite <- IHn. reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [| n'].
+  reflexivity.
+  simpl. rewrite -> IHn'. reflexivity.
+Qed.
+ 
 
 Lemma myOdd_lemma : forall n,
   myOdd n -> (exists m, n = 2*m + 1).
@@ -56,13 +91,34 @@ Proof.
       exists 0. reflexivity.
       inversion IHmyOdd.
       exists (x + 1).
-      assert (HH : n = 2 * x + 1 -> S (S n) = 2 * (x + 1) + 1).
-        admit.
-      apply HH.
-      apply H0.
+      rewrite -> expand_S.
+      rewrite -> expand_S.
+      rewrite -> H0.
+      assert (HH : 2 * (x + 1) = 2 * x + 2).
+        simpl.
+        rewrite -> plus_0_r.
+        simpl.
+        destruct x.
+        simpl. reflexivity.
+        rewrite -> plus_0_r.
+        rewrite -> plus_swap.
+        rewrite <- plus_assoc.
+        assert (two : 2 = 1 + 1). reflexivity.
+        rewrite <- two.
+        rewrite <- plus_assoc.
+        reflexivity.
+        rewrite <- plus_assoc.
+        assert (two : 2 = 1 + 1). reflexivity.
+        rewrite <- two.
+        rewrite -> HH.
+        rewrite <- plus_assoc.
+        simpl.
+        assert (three : 3 = 2 + 1). reflexivity.
+        rewrite -> three.
+        rewrite -> plus_assoc.
+        reflexivity.
 Qed.
-
-
+        
 
 (* 2. Define myOdd2 and myEven2 mutually inductively. 
    Prove the lemma: forall n, myOdd2 n -> exists m, n = 2*m +1. *)
