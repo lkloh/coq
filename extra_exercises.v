5a869875c1e70654be5278e2d43e1418a357e137
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-08-12 12:26:03 -0400

    changed

--- extra_exercises.v
+++ extra_exercises.v
@@ -81,9 +81,8 @@ Proof.
   reflexivity.
   simpl. rewrite -> IHn'. reflexivity.
 Qed.
- 
 
-Lemma myOdd_lemma : forall n,
+Lemma myOdd_lemma' : forall n,
   myOdd n -> (exists m, n = 2*m + 1).
 Proof.
   intros n.
@@ -91,48 +90,24 @@ Proof.
   induction n as [| n'].
     inversion H.
     induction H as [| ].
-      exists 0. reflexivity.
+      exists 0.
+        reflexivity.
       inversion IHmyOdd.
-      exists (x + 1).
-      rewrite -> expand_S.
-      rewrite -> expand_S.
-      rewrite -> H0.
-      assert (HH : 2 * (x + 1) = 2 * x + 2).
+        exists (S x).
+        rewrite -> H0.
         simpl.
         rewrite -> plus_0_r.
-        simpl.
-        destruct x.
-        simpl. reflexivity.
-      rewrite -> plus_0_r.
-      rewrite -> plus_swap.
-      rewrite <- plus_assoc.
-      assert (two : 2 = 1 + 1). reflexivity.
-      rewrite <- two.
-      rewrite <- plus_assoc.
-      reflexivity.
-      rewrite <- plus_assoc.
-      assert (two : 2 = 1 + 1). reflexivity.
-      rewrite <- two.
-      rewrite -> HH.
-      rewrite <- plus_assoc.
-      simpl.
-      assert (three : 3 = 2 + 1). reflexivity.
-      rewrite -> three.
-      rewrite -> plus_assoc.
-      reflexivity.
+        rewrite <- plus_assoc.
+        rewrite -> plus_n_Sm.
+        apply f_equal.
+        rewrite -> plus_0_r.
+        rewrite -> expand_S.
+        reflexivity.
 Qed.
         
 
 (* 2. Define myOdd2 and myEven2 mutually inductively. 
    Prove the lemma: forall n, myOdd2 n -> exists m, n = 2*m +1. *)
-
-(*
-  Inductive myEven2: nat -> Set :=
-    | e2_1 : myEven2 0
-    | e2_IH : forall n : nat, myOdd2 n -> myEven2 (S n)
-  with myOdd2 : nat -> Set :=
-    | o2_IH : forall n : nat, myEven2 n -> myOdd2 (S n).
-*)
 
 Inductive myOdd2: nat -> Set :=
   | o2_1 : myOdd2 1
@@ -151,22 +126,7 @@ Proof. apply e2_IH. apply o2_IH. apply e2_IH. apply o2_1. Qed.
 
 Scheme myOdd2_mut := Induction for myOdd2 Sort Prop
   with myEven2_mut := Induction for myEven2 Sort Prop.
-Check myOdd2_mut.
-
-Lemma myOdd2_easier : forall n,
-  myOdd2 n -> ble_nat 1 n = true.
-Proof.
-  apply (myOdd2_mut
-    (fun n : nat => (fun mo => ble_nat 1 n = true))
-    (fun n : nat => (fun me => ble_nat 0 n = true))
-  ).
-  reflexivity.
-  intros. reflexivity.
-  intros. reflexivity.
-Qed.
-
-
-    
+Check myOdd2_mut.    
 
 Lemma myOdd2_lemma : forall n,
   myOdd2 n -> (exists m, n = 2*m + 1).
@@ -187,23 +147,23 @@ Proof.
     apply f_equal.
     rewrite -> plus_0_r.
     rewrite <- plus_assoc.
-    rewrite <- plus_n_Sm.
-    rewrite -> plus_0_r.
     rewrite -> plus_n_Sm.
+    rewrite -> plus_0_r.
     rewrite -> expand_S.
-    rewrite -> plus_assoc.
     reflexivity.
 Qed.
 
-
-
-Theorem ex_falso_quodlibet : forall (P:Prop),
-  False -> P.
+Lemma myOdd2_easier : forall n,
+  myOdd2 n -> ble_nat 1 n = true.
 Proof.
-  intros P contra.
-  inversion contra.
+  apply (myOdd2_mut
+    (fun n : nat => (fun mo => ble_nat 1 n = true))
+    (fun n : nat => (fun me => ble_nat 0 n = true))
+  ).
+  reflexivity.
+  intros. reflexivity.
+  intros. reflexivity.
 Qed.
-
 
 (* simpler example test *)
 
@@ -218,15 +178,6 @@ with odd_ind_2 := Minimality for odd Sort Prop.
 
 Check even_ind_2.
 
-Fixpoint ble_nat (n m : nat) : bool :=
-  match n with
-  | O => true
-  | S n' =>
-      match m with
-      | O => false
-      | S m' => ble_nat n' m'
-      end
-  end.
 
 Lemma even_pos: forall n,
   even n -> ble_nat 0 n = true.
